88f94e97bc5b8c224ccb7ea55cb4f1f664cb2550
www-139-cn <112906505@qq.com>
2022-03-07 22:21:45 +0800

    右旋完成

--- rbtutils/rbtutil.go
+++ rbtutils/rbtutil.go
@@ -11,17 +11,30 @@ import (
 // RBTDemo 红黑树演示
 func RBTDemo() {
 	RBTCreat()
+	err := errors.New("出错，本节点是空！")
+
 	ShowTree(global.Root)
-	//LeftRotate(global.Root.Left) // 测通
-	//LeftRotate(global.Root.Right) // 测通
-	LeftRotate(global.Root) // 测试根
+
+	//err = LeftRotate(global.Root.Left) // 测通
+	//err = LeftRotate(global.Root.Right) // 测通
+	//err = LeftRotate(global.Root) // 测试根
+
+	//err = RightRotate(global.Root.Left) // 测通
+	//err = RightRotate(global.Root.Right) // 测通
+	//err = RightRotate(global.Root) // 测试根
+
+	err = RightRotate(global.Root.Right.Right.Right) // 测通
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
 	ShowTree(global.Root)
 	//global.Root.Left.ShowTree()
 }
 
 // RBTCreat 红黑树创建
 func RBTCreat() {
-
 	// 定义
 	global.Root = rbtmodels.NewRBTNode(false, 1, "1宋江", nil, nil, nil)
 	DemoPush(global.Root, true, rbtmodels.NewRBTNode(false, 2, "2卢俊义", nil, nil, nil))
@@ -34,6 +47,10 @@ func RBTCreat() {
 	DemoPush(global.Root.Left.Left, false, rbtmodels.NewRBTNode(false, 9, "9华融", nil, nil, nil))
 	DemoPush(global.Root.Left.Right, true, rbtmodels.NewRBTNode(false, 10, "10柴进", nil, nil, nil))
 	DemoPush(global.Root.Left.Right, false, rbtmodels.NewRBTNode(false, 11, "11李应", nil, nil, nil))
+	DemoPush(global.Root.Right.Left, true, rbtmodels.NewRBTNode(false, 12, "12朱仝", nil, nil, nil))
+	DemoPush(global.Root.Right.Left, false, rbtmodels.NewRBTNode(false, 13, "13鲁智深", nil, nil, nil))
+	DemoPush(global.Root.Right.Right, true, rbtmodels.NewRBTNode(false, 14, "14武松", nil, nil, nil))
+	DemoPush(global.Root.Right.Right, false, rbtmodels.NewRBTNode(false, 15, "15董平", nil, nil, nil))
 
 }
 
@@ -103,8 +120,9 @@ func ShowOneNode(n *rbtmodels.RBTNode) {
 	}
 }
 
+// LeftRotate 左旋
+// @param p point 旋转的支点
 /*
- * 以p为支点左旋
  *   parent                 parent
  *     |                      |
  *     p                      pr
@@ -112,12 +130,7 @@ func ShowOneNode(n *rbtmodels.RBTNode) {
  *  pl   pr                 p    rr
  *      /  \               / \
  *     rl   rr            pl  rl
- *
  */
-
-// LeftRotate 左旋转
-// @param root 树的根
-// @param p point 旋转的支点
 func LeftRotate(p *rbtmodels.RBTNode) (err error) {
 	if p == nil {
 		return errors.New("出错，本节点是空！")
@@ -128,8 +141,8 @@ func LeftRotate(p *rbtmodels.RBTNode) (err error) {
 
 	parent := p.Parent // 父亲
 	pSelf := p         // 本身
-	pr := p.Right      // 右儿子
-	rl := pr.Left      // 右儿子的左孙子
+	pr := p.Right      // 右儿子（升级）
+	rl := pr.Left      // 右儿子的左孙子（断枝重连）
 
 	// 下方需要判断 p 是否root
 	if parent != nil { // p不是root，，还需要分析，p在父亲的左还是右
@@ -139,23 +152,74 @@ func LeftRotate(p *rbtmodels.RBTNode) (err error) {
 			parent.Right = pr // 1.2 父亲右指向：
 		}
 		pr.Parent = parent // 2.1 (升级的)pr上指向：
-		pr.Left = pSelf    // 2.2 (升级的)pr左指向：
 	} else { // p是root
 		global.Root = pr         // 1.1 + 1.2 父亲的指向。其实是root指向pr
 		global.Root.Parent = nil // 2.1 (升级的)pr上指向：
-		pr.Left = pSelf          // 2.2 (升级的)pr左指向：
 	}
 
 	// 下方p是否root都要执行
+	pr.Left = pSelf // 2.2 (升级的)pr左指向：
 	// 2.3 (升级的)pr右指向：不动
 	pSelf.Parent = pr // 3.1 pSelf上指向：
 	// 3.2 pSelf左指向：不动
 	pSelf.Right = rl // 3.3 pSelf右指向：rl
 	if rl != nil {
-		rl.Parent = pSelf // 4.1 pr.Left上指向：
+		rl.Parent = pSelf // 4.1 rl上指向：
 	}
-	// 4.2 pr.Left左指向：不动
-	// 4.3 pr.Left右指向：不动
+	// 4.2 rl左指向：不动
+	// 4.3 rl右指向：不动
+
+	return err
+}
+
+// RightRotate 右旋
+// @param p point 旋转的支点
+/*
+ *     parent                 parent
+ *       |                      |
+ *       p                     pl
+ *      / \        ===>    	  /  \
+ *    pl   pr                LL   P
+ *   /  \                        / \
+ *  ll   lr                     lr  pr
+ */
+func RightRotate(p *rbtmodels.RBTNode) (err error) {
+	if p == nil {
+		return errors.New("出错，本节点是空！")
+	}
+	if p.Left == nil {
+		return errors.New("出错，本节点左儿子是空！")
+	}
+
+	parent := p.Parent // 父亲
+	pSelf := p         // 本身
+	pl := p.Left       // 左儿子（升级）
+	lr := pl.Right     // 左儿子的右孙子（断枝重连）
+
+	// 下方需要判断 p 是否root
+	if parent != nil { // p不是root，，还需要分析，p在父亲的左还是右
+		if parent.Left == pSelf { // p 在父亲左手
+			parent.Left = pl // 1.1 父亲左指向：
+		} else { // p 在父亲右手
+			parent.Right = pl // 1.2 父亲右指向：
+		}
+		pl.Parent = parent // 2.1 (升级的)pl上指向：
+	} else { // p是root
+		global.Root = pl         // 1.1 + 1.2 父亲的指向。其实是root指向pr
+		global.Root.Parent = nil // 2.1 (升级的)pr上指向：
+	}
+
+	// 下方p是否root都要执行
+	pl.Right = pSelf // 2.2 (升级的)pl右指向：
+	// 2.3 (升级的)pl左指向：不动
+	pSelf.Parent = pl // 3.1 pSelf上指向：
+	// 3.2 pSelf右指向：不动
+	pSelf.Left = lr // 3.3 pSelf左指向：lr
+	if lr != nil {
+		lr.Parent = pSelf // 4.1 lr上指向：
+	}
+	// 4.2 lr左指向：不动
+	// 4.3 lr右指向：不动
 
 	return err
 }
